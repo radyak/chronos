5694fe36d5c05dd20e384c72b741fb8efa6c46c4
radyak <florian.vogel84@gmx.net>
2024-05-19 17:19:52 +0200

    #2: Add sort to entries list

--- chronos-frontend/src/app/modules/public/views/public-timeline/public-timeline.component.ts
+++ chronos-frontend/src/app/modules/public/views/public-timeline/public-timeline.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {TagsService} from "../../service/tags.service";
-import {combineLatest, map, Observable, retry, shareReplay} from "rxjs";
+import {combineLatest, map, Observable, shareReplay} from "rxjs";
 import {Tag} from "../../../../model/tag.model";
 import {ActivatedRoute, Params, Router} from "@angular/router";
 import {EntriesService} from "../../service/entries.service";
@@ -12,17 +12,31 @@ import {
   faChartGantt,
   faMagnifyingGlassMinus,
   faMagnifyingGlassPlus,
-  faPlus, faTable,
+  faPlus, faSort, faSortDown, faSortUp,
+  faTable,
   faTrash
 } from "@fortawesome/free-solid-svg-icons"
 import {getEarliestStartDateRange} from "../../../../util/date-range.utils";
 import {QueryDrivenComponent} from "../../../../common/query-driven-component.directive";
 import {IconDefinition} from "@fortawesome/fontawesome-svg-core";
 
-interface DispayOption {
+interface DisplayOption {
   id: string,
   label: string,
   icon: IconDefinition
+}
+
+interface EntrySortableProperty {
+  key: 'title' | 'subTitle' | 'start' | 'end';
+  label: string;
+  compare: (e1: Entry, e2: Entry) => number;
+}
+
+type SortDirection = 'asc' | 'desc';
+
+interface SortConfig {
+  property: EntrySortableProperty,
+  direction: SortDirection
 }
 
 @Component({
@@ -55,7 +69,7 @@ export class PublicTimelineComponent extends QueryDrivenComponent implements OnI
 
   display: string = 'table';
 
-  displayOptions: Array<DispayOption> = [
+  displayOptions: Array<DisplayOption> = [
     {
       id: 'table',
       icon: faTable,
@@ -67,6 +81,47 @@ export class PublicTimelineComponent extends QueryDrivenComponent implements OnI
       label: 'Timeline'
     },
   ];
+
+  sortOptions: Array<EntrySortableProperty> = [
+    {
+      key: 'title',
+      label: 'Title',
+      compare: (e1: Entry, e2: Entry) => {
+        if ((e1.title || '') < (e2.title || ''))
+          return 1;
+        if ((e1.title || '') > (e2.title || ''))
+          return -1;
+        return 0;
+      }
+    },
+    {
+      key: 'subTitle',
+      label: 'Subtitle',
+      compare: (e1: Entry, e2: Entry) => {
+        if ((e1.subTitle || '') < (e2.subTitle || ''))
+          return 1;
+        if ((e1.subTitle || '') > (e2.subTitle || ''))
+          return -1;
+        return 0;
+      }
+    },
+    {
+      key: 'start',
+      label: 'Earliest start date',
+      compare: (e1: Entry, e2: Entry) => {
+        return (getEarliestStartDateRange(e1.dateRanges).start! > getEarliestStartDateRange(e2.dateRanges).start! ? 1 : -1)
+      }
+    },
+    {
+      key: 'end',
+      label: 'Latest end date',
+      compare: (e1: Entry, e2: Entry) => {
+        return (getEarliestStartDateRange(e1.dateRanges).end! > getEarliestStartDateRange(e2.dateRanges).end! ? 1 : -1)
+      }
+    }
+  ];
+
+  sortConfig?: SortConfig;
 
   get selectedColorTagCategory(): TagCategory | undefined {
     return this.tagCategories.find(c => c.id === this.colorCategoryId);
@@ -147,8 +202,12 @@ export class PublicTimelineComponent extends QueryDrivenComponent implements OnI
         to: this.to
       }).subscribe(entries =>
         this.entries = [
-          entries.sort((e1, e2) => ((
-            getEarliestStartDateRange(e1.dateRanges).start! > getEarliestStartDateRange(e2.dateRanges).start!) ? 1 : -1)
+          entries.sort((e1, e2) => {
+              if (!this.sortConfig) {
+                return 0;
+              }
+              return this.sortConfig.property.compare(e1, e2) * (this.sortConfig.direction === 'asc' ? 1 : -1)
+            }
           )
         ]
       );
@@ -165,9 +224,12 @@ export class PublicTimelineComponent extends QueryDrivenComponent implements OnI
           })
         )
       ).subscribe(entries => {
-          entries.forEach(entry => entry.sort((e1, e2) =>
-            getEarliestStartDateRange(e1.dateRanges).start! > getEarliestStartDateRange(e2.dateRanges).start! ? 1 : -1)
-          );
+          entries.forEach(entry => entry.sort((e1, e2) => {
+              if (!this.sortConfig) {
+                return 0;
+              }
+              return this.sortConfig.property.compare(e1, e2) * (this.sortConfig.direction === 'asc' ? 1 : -1)
+          }));
           this.entries = entries;
         }
       );
@@ -190,6 +252,14 @@ export class PublicTimelineComponent extends QueryDrivenComponent implements OnI
         this.colorCategoryId = parseInt(params['color-category']) || undefined;
         this.showSearchInputs = params['show-search-inputs'];
         this.display = params['display'] || 'table';
+
+        const sortBy: EntrySortableProperty | undefined = this.sortOptions.find(o => o.key === params['sortBy']);
+        if (sortBy) {
+          this.sortConfig = {
+            property: sortBy,
+            direction: params['sortOrder']
+          }
+        }
         return;
       })
     )
@@ -211,7 +281,9 @@ export class PublicTimelineComponent extends QueryDrivenComponent implements OnI
       'to': this.to || null,
       'color-category': this.colorCategoryId || null,
       'show-search-inputs': this.showSearchInputs || null,
-      'display': this.display || null
+      'display': this.display || null,
+      'sortBy': this.sortConfig?.property.key || null,
+      'sortOrder': this.sortConfig?.direction || null,
     }
   }
 
@@ -220,8 +292,36 @@ export class PublicTimelineComponent extends QueryDrivenComponent implements OnI
     this.updateSearchParams();
   }
 
+  sort(property: EntrySortableProperty | null, direction?: SortDirection): void {
+    if (!property) {
+      delete this.sortConfig;
+    } else {
+      this.sortConfig = {
+        property: property,
+        direction: direction || this.sortConfig?.direction || 'asc'
+      }
+    }
+    this.updateSearchParams();
+  }
+
   flatEntries(): Array<Entry> {
     return this.entries.reduce((p, c) => p.concat(c));
+  }
+
+  sortIcon(): IconDefinition {
+    switch (this.sortConfig?.direction) {
+      case 'asc': return faSortUp;
+      case 'desc': return faSortDown;
+      default: return faSort;
+    }
+  }
+
+  rotateSort(): void {
+    if (!this.sortConfig) {
+      return;
+    }
+    this.sortConfig.direction = this.sortConfig.direction === 'asc' ? 'desc' : 'asc';
+    this.updateSearchParams();
   }
 
 }
