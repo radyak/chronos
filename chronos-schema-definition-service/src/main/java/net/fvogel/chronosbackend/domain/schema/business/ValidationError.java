f454effcfb71d1473b98dae15f70d6da21bf266b
radyak <florian.vogel84@gmx.net>
2026-01-05 00:59:45 +0100

    feat(GH-1): Add unique key validation

---
+++ chronos-schema-definition-service/src/main/java/net/fvogel/chronosbackend/domain/schema/business/ValidationError.java
@@ -0,0 +1,19 @@
+package net.fvogel.chronosbackend.domain.schema.business;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+public class ValidationError {
+    private String field;
+    private String constraint;
+    private String message;
+    private Map<String, Object> arguments;
+
+    public ValidationError(String field, String constraint, String message) {
+        this(field, constraint, message, null);
+    }
+}
