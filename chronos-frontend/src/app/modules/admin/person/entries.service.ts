d061a184fb69ee49aeaa05c1f4b4f896e9edca14
radyak <florian.vogel84@gmx.net>
2026-01-10 15:07:52 +0100

    fix(GH-1): Fix frontend build (thx IntelliJ CE -_-)

--- chronos-frontend/src/app/modules/admin/person/entries.service.ts
+++ chronos-frontend/src/app/modules/admin/person/entries.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
-import { Entity as Person } from 'src/app/common/model/domain/entityPO.model';
+import { Entity as Person } from 'src/app/common/model/domain/entity.model';
 
 export interface PersonSearchParams {
   from?: number;
