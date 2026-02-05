13fb3b65159ed1e54f886c0a430934935a38576f
radyak <florian.vogel84@gmx.net>
2026-02-05 11:17:03 +0100

    dev(GH-1): Fix test-data setup

--- chronos-schema-definition-service/src/main/java/net/fvogel/chronosbackend/config/dev/TestDataImportConfig.java
+++ chronos-schema-definition-service/src/main/java/net/fvogel/chronosbackend/config/dev/TestDataImportConfig.java
@@ -1,10 +1,6 @@
 package net.fvogel.chronosbackend.config.dev;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import net.fvogel.chronosbackend.domain.schema.persistence.model.entity.EntityPO;
-import net.fvogel.chronosbackend.domain.schema.persistence.model.relation.RelationPO;
-import net.fvogel.chronosbackend.domain.schema.service.SchemaService;
+import net.fvogel.chronosbackend.shared.dev.TestDataManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
@@ -12,60 +8,17 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 
 @Profile("test-data")
 @Configuration
 public class TestDataImportConfig {
 
-    private static final ObjectMapper mapper = new ObjectMapper();
-
     @Autowired
-    SchemaService service;
+    TestDataManager testDataManager;
 
     @EventListener(ApplicationReadyEvent.class)
     public void importTestData() throws IOException {
-        this.importTestData("testdata/test-schema.json");
-    }
-
-    public void importTestData(String resourcePath) throws IOException {
-
-        // READ & DESERIALIZE JSON
-        InputStream is = TestDataImportConfig.class
-                .getClassLoader()
-                .getResourceAsStream(resourcePath);
-
-        if (is == null) {
-            throw new IllegalStateException("Resource not found");
-        }
-
-        List<EntityPO> entityPOs = mapper.readValue(is, new TypeReference<List<EntityPO>>() {
-        });
-
-        // CREATE ENTITIES FIRST, COLLECT RELATIONS FOR SUBSEQUENT, SEPARATE CREATION
-        List<RelationPO> relationPOs = new ArrayList<>();
-        entityPOs.forEach(entityPO -> {
-            relationPOs.addAll(entityPO.getRelations());
-            entityPO.getRelations().clear();
-            this.service.save(entityPO);
-        });
-
-        // NOW CREATE RELATIONS
-        relationPOs.forEach(relationPO -> {
-            relationPO.setTarget(findByKey(relationPO.getTarget().getKey(), entityPOs));
-            relationPO.setSource(findByKey(relationPO.getSource().getKey(), entityPOs));
-            service.save(relationPO);
-        });
-
-    }
-
-    private EntityPO findByKey(String key, List<EntityPO> entityPOs) {
-        return entityPOs.stream()
-                .filter(entityPO -> key.equals(entityPO.getKey()))
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException("No EntityPO saved with key '" + key + "'"));
+        this.testDataManager.importTestData();
     }
 
 }
--- chronos-schema-definition-service/src/test/java/net/fvogel/chronosbackend/it/admin/AdminApiEntityManagementIntegrationTest.java
+++ chronos-schema-definition-service/src/test/java/net/fvogel/chronosbackend/it/admin/AdminApiEntityManagementIntegrationTest.java
@@ -3,6 +3,8 @@ package net.fvogel.chronosbackend.it.admin;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.fvogel.chronosbackend.commons.security.DevJwtGenerator;
 import net.fvogel.chronosbackend.domain.schema.persistence.repository.EntityPORepository;
+import net.fvogel.chronosbackend.shared.dev.TestDataManager;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.io.IOException;
 import java.util.Set;
 
 import static net.fvogel.chronosbackend.testutils.EntityBuilder.createMinimalEntity;
@@ -23,7 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles({"test", "test-data"})
+@ActiveProfiles("test")
 public class AdminApiEntityManagementIntegrationTest {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
@@ -34,15 +37,23 @@ public class AdminApiEntityManagementIntegrationTest {
     @Autowired
     EntityPORepository entityPORepository;
     @Autowired
+    private TestDataManager testDataManager;
+    @Autowired
     private WebApplicationContext context;
     private MockMvc mvc;
 
     @BeforeEach
-    public void setup() {
+    public void setup() throws IOException {
+        testDataManager.importTestData();
         mvc = MockMvcBuilders
                 .webAppContextSetup(context)
                 .apply(SecurityMockMvcConfigurers.springSecurity())
                 .build();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        testDataManager.clearAll();
     }
 
     @Test
