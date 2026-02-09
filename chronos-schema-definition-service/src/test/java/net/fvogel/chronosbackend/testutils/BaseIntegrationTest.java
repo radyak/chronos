aaaabeb480e16a902f17c80161ea84d5f6fe28de
radyak <florian.vogel84@gmx.net>
2026-02-09 23:00:46 +0100

    dev(GH-1): Refactor integration tests and add more cases

---
+++ chronos-schema-definition-service/src/test/java/net/fvogel/chronosbackend/testutils/BaseIntegrationTest.java
@@ -0,0 +1,88 @@
+package net.fvogel.chronosbackend.testutils;
+
+import net.fvogel.chronosbackend.commons.security.DevJwtGenerator;
+import net.fvogel.chronosbackend.domain.schema.persistence.model.entity.EntityPO;
+import net.fvogel.chronosbackend.domain.schema.persistence.repository.EntityPORepository;
+import net.fvogel.chronosbackend.shared.dev.TestDataManager;
+import org.hibernate.Hibernate;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.IOException;
+import java.util.Set;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public abstract class BaseIntegrationTest {
+
+    @Value("${app.auth.admin-role}")
+    protected String adminRole;
+
+    @Autowired
+    protected DevJwtGenerator jwtGenerator;
+
+    @Autowired
+    protected TestDataManager testDataManager;
+
+    @Autowired
+    protected WebApplicationContext context;
+
+    @Autowired
+    protected EntityPORepository entityPORepository;
+
+    protected MockMvc mvc;
+
+    @BeforeEach
+    public void setup() throws IOException {
+        testDataManager.importTestData();
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        testDataManager.clearAll();
+    }
+
+    protected String adminAuthHeader() {
+        return authHeader("admin", Set.of(adminRole));
+    }
+
+    protected String authHeader(String username) {
+        return authHeader(username, Set.of());
+    }
+
+    protected String authHeader(String username, Set<String> roles) {
+        return "Bearer " + jwtGenerator.generateJWT(username, roles);
+    }
+
+    protected ResultActions getEntity(String entityKey) throws Exception {
+        return mvc.perform(MockMvcRequestBuilders.get("/api/schema/{key}", entityKey));
+    }
+
+    protected EntityPO loadEntity(String entityKey) {
+        EntityPO entity = entityPORepository.findByKey("Person").get();
+        Hibernate.initialize(entity);
+        entity.getRelations().forEach(relation -> {
+            relation.setSource(null);
+            EntityPO newTarget = new EntityPO();
+            newTarget.setId(relation.getTarget().getId());
+            relation.setTarget(newTarget);
+        });
+
+        return entity;
+    }
+
+}
