068c589284128afee75bd413fe0ba38a8d07427a
radyak <florian.vogel84@gmx.net>
2026-03-21 15:57:49 +0100

    chore(GH-1): Fix issues after updates

--- chronos-frontend/src/app/modules/public/public-overview/public-overview.component.ts
+++ chronos-frontend/src/app/modules/public/public-overview/public-overview.component.ts
@@ -18,11 +18,6 @@ import { EntitiesService } from '../../admin/person/entities.service';
   standalone: true,
   imports: [
     CommonModule,
-    
-// TODO: `HttpClientModule` should not be imported into a component directly.
-// Please refactor the code to add `provideHttpClient()` call to the provider list in the
-// application bootstrap logic and remove the `HttpClientModule` import from this component.
-HttpClientModule,
     RouterModule,
     NgbTypeaheadModule,
     FormsModule,
