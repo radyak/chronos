9945cbe0aa1dae71042c10469eb9c839b2db364a
radyak <florian.vogel84@gmx.net>
2026-04-10 23:53:17 +0200

    feat(GH-1): Add edit attribute form errors and allowedValues input

---
+++ chronos-frontend/src/app/common/util/form.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FormService {
+  
+  private readonly ERROR_MAP: Record<string, (key: string, params?: any) => string> = {
+    required: (key: string) => `"${key}" is required`,
+    minlength: (key: string, params: any) => `"${key}" must have at least ${params.requiredLength} characters`,
+    maxlength: (key: string, params: any) => `"${key}" may have at most ${params.requiredLength} characters`,
+  }
+
+  public extractErrors(field: string, label: string, form: FormGroup): string[] {
+    const errors = form.get(field)?.errors;
+    if (!errors) {
+      return [];
+    }
+    return Object.keys(errors).map(errorKey => {
+      const errorGenerator = this.ERROR_MAP[errorKey];
+      const errorParams = errors[errorKey];
+      return errorGenerator?.(label, errorParams);
+    }).filter(error => !!error);
+  }
+
+}
