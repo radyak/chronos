85f89b6f8fd11025ef42c72d1c7b0a2290a815fd
radyak <florian.vogel84@gmx.net>
2026-04-10 23:53:17 +0200

    feat(GH-1): Add edit attribute form errors and allowedValues input

--- chronos-frontend/src/app/modules/admin/views/admin/schema/edit-entity/edit-entity-attribute-dialog/edit-entity-attribute-dialog.component.ts
+++ chronos-frontend/src/app/modules/admin/views/admin/schema/edit-entity/edit-entity-attribute-dialog/edit-entity-attribute-dialog.component.ts
@@ -1,13 +1,18 @@
-import { Component, effect, inject, model, ModelSignal } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, computed, effect, inject, model, ModelSignal, signal, WritableSignal } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, NgModel, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { AttributeAO } from 'src/app/common/model/domain/schema/admin/attribute.ao';
 import { AttributeTypeDTO } from 'src/app/common/model/domain/schema/attribute-type.dto';
+import { FormService } from 'src/app/common/util/form.service';
 
 @Component({
   selector: 'chronos-edit-entity-attribute-dialog',
   imports: [
     ReactiveFormsModule,
+    FontAwesomeModule,
+    FormsModule
   ],
   templateUrl: './edit-entity-attribute-dialog.component.html',
   styleUrl: './edit-entity-attribute-dialog.component.scss',
@@ -17,16 +22,22 @@ export class EditEntityAttributeDialogComponent {
   // Dependencies
   private fb = inject(FormBuilder);
   private activeModal = inject(NgbActiveModal);
+  private formService = inject(FormService);
 
   // Input
   protected attribute: ModelSignal<AttributeAO | undefined> = model();
 
+  // Icons
+  protected deleteIcon = faTrash;
+  protected addIcon = faPlus;
+
   // Form & controls
   protected form!: FormGroup;
-  // type AttributeTypeKey = keyof typeof AttributeTypeDTO
+  protected submitted = false;
   protected types: AttributeTypeDTO[] = Object.values(AttributeTypeDTO)
       .filter(t => typeof t !== "number")
       .map(t => t as unknown as AttributeTypeDTO);
+  protected newAllowedValue: WritableSignal<string> = signal("");
 
   // Init
   constructor() {
@@ -82,15 +93,52 @@ export class EditEntityAttributeDialogComponent {
     });
   }
 
-  confirm(): void {
+  protected confirm(): void {
+    this.submitted = true;
     if (this.form.invalid) {
+      this.form.markAllAsTouched();
       return;
     }
     const attributeResult = this.form.getRawValue();
+    attributeResult.allowedValues = this.attribute()?.allowedValues;
     this.activeModal.close(attributeResult);
   }
 
-  cancel(): void {
+  protected cancel(): void {
     this.activeModal.dismiss()
+  }
+
+  protected isInvalid(field: string): boolean {
+    const ctrl = this.form.get(field);
+    return !!(ctrl?.invalid && (this.submitted || ctrl?.touched));
+  }
+
+  protected errors(field: string, label: string): string[] {
+    return this.formService.extractErrors(field, label, this.form);
+  }
+
+  protected isNew = computed(() => !this.attribute()?.id);
+
+  protected deleteAllowedValue(allowedValue: string): void {
+    let index = this.attribute()?.allowedValues?.indexOf(allowedValue);
+    if (index === undefined || index === -1) {
+      return;
+    }
+    this.attribute()?.allowedValues?.splice(index, 1);
+  }
+
+  // protected createAllowedValue(): void {
+  //   this.newAllowedValue.set("");
+  // }
+
+  protected addAllowedValue(): void {
+    const newAllowedValue = this.newAllowedValue();
+    const attribute = this.attribute();
+    if (!newAllowedValue || !attribute || attribute?.allowedValues?.find(a => a === newAllowedValue)) {
+      return;
+    }
+    attribute!.allowedValues = this.attribute()!.allowedValues ?? [];
+    attribute!.allowedValues?.push(newAllowedValue!);
+    this.newAllowedValue.set("");
   }
 }
